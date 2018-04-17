6e2fe7a8b0dcbce1033265e963ac0a85230167b4
syoung <syoung1394@naver.com>
2018-04-17 17:28:13 +0900

    add function

--- src/main/java/kr/ac/jejunu/ProductDao.java
+++ src/main/java/kr/ac/jejunu/ProductDao.java
@@ -4,8 +4,7 @@ import java.sql.*;
 
 public class ProductDao {
     public Product get(Long id) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/jeju", "jeju", "jejupw");
+        Connection connection = getConnection();
 
         PreparedStatement preparedStatement = connection.prepareStatement("select * from product where id = ?");
         preparedStatement.setLong(1, id);
@@ -18,12 +17,35 @@ public class ProductDao {
         product.setTitle(resultSet.getString("title"));
         product.setPrice(resultSet.getInt("price"));
 
-        //자원을 해지한다.
-        resultSet.close();cmd
-                
+        resultSet.close();
+
         preparedStatement.close();
         connection.close();
 
         return product;
+    }
+    public Long insert(Product product) throws SQLException, ClassNotFoundException{
+        Connection connection = getConnection();
+
+        PreparedStatement preparedStatement = connection.prepareStatement("insert into product(title, price) values(?, ?)");
+        preparedStatement.setString(1, product.getTitle());
+        preparedStatement.setInt(2, product.getPrice());
+
+        preparedStatement.executeUpdate();
+
+        preparedStatement=connection.prepareStatement("select last_insert_id()");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+
+        Long id=resultSet.getLong(1);
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+
+        return id;
+    }
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        return DriverManager.getConnection("jdbc:mysql://localhost/jeju", "root", "qwe123");
     }
 }
--- src/test/java/kr/ac/jejunu/ProductDaoTest.java
+++ src/test/java/kr/ac/jejunu/ProductDaoTest.java
@@ -1,18 +1,26 @@
 package kr.ac.jejunu;
 
+import org.junit.Before;
 import org.junit.Test;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 import java.sql.SQLException;
 
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class ProductDaoTest {
+    private ProductDao productDao;
+
+    @Before
     public void setup() {
+        productDao=new ProductDao();
     }
 
     @Test
     public void get() throws SQLException, ClassNotFoundException {
-        ProductDao productDao = new ProductDao();
         Long id = 1L;
         String title = "제주감귤";
         Integer price = 15000;
@@ -21,5 +29,18 @@ public class ProductDaoTest {
         assertEquals(id, product.getId());
         assertEquals(title, product.getTitle());
         assertEquals(price, product.getPrice());
+    }
+
+    @Test
+    public void add() throws SQLException, ClassNotFoundException{
+        Product product=new Product();
+        product.setTitle("AA");
+        product.setPrice(200);
+        long id=productDao.insert(product);
+
+        Product insertedProduct = productDao.get(id);
+        assertThat(insertedProduct.getId(),is(id));
+        assertThat(insertedProduct.getTitle(),is(product.getTitle()));
+        assertThat(insertedProduct.getPrice(),is(product.getPrice()));
     }
 }
